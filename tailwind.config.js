9e5d4b8a4e56dc0f85d0204f0921bc8d57d28991
elevatebart <bart@cypress.io>
2022-12-27 13:38:44 -0600

    add tailwind and setup system

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.vue", "./src/*.js", "./src/*.vue"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
   entry: './src/index.ts',
@@ -16,19 +17,6 @@ const config = {
         loader: 'vue-loader'
       },
       {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
-      },
-      {
         test: /\.ts(x)?$/,
         loader: 'ts-loader',
         exclude: /node_modules/,
@@ -37,7 +25,29 @@ const config = {
             /\.vue$/
           ]
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
     ]
   },
   resolve: {
@@ -53,7 +63,8 @@ const config = {
     new HtmlWebpackPlugin({
       templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
       filename: 'index.html',
-    })
+    }),
+    new MiniCssExtractPlugin(),
   ]
 };
 
